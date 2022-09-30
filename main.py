69c2cb3a1e94c1bd81f13e8958df2d9722f0fe65
eti-du <92732299+eti-du@users.noreply.github.com>
2022-09-30 09:56:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Personnage:
         #Augmente d’un niveau tous les 10 xp
         #Exemple 30xp = niveau 3
         self.xp += xp
-        self.niveau = xp//10
+        self.niveau = self.xp//10# ???
     def estVivant(self):
         #retourne vrai si le personnage est vivant
         return self.vie>0
@@ -51,19 +51,93 @@ class Guerrier(Personnage):
         if adversaire.estMort():
             self.augmenterForce()
         print("degat sur le mechant",degats)
+class Magicien(Personnage):
+    def __init__(self,nom,mana,vie,xp,niveau):
+        super().__init__(nom,vie,xp,niveau)
+        self.maxMana=mana
+        self.mana=mana
+    def augmenterMana(self):#augmente de 10 self.maxMana
+        self.maxMana += 10
+    def ajouterMana(self):#ajoute 1 en self.mana sans dépasser self.maxMana
+        if self.mana + 1 <= self.maxMana:
+            self.mana += 1
+    def retirerMana(self,mana):
+        #retire mana à self.mana sans descendre en dessous de 0
+        #retourne vrai si le magicien à lancé un sort
+        #retourne faux si le magicien nen peut plus lancer de sort.
+        if self.mana -1 >= 0:
+            self.mana -= 1
+            return True
+        else:
+            return False
+    def combat(self,adversaire):
+        attaque=randint(1, 4)
+        degats=attaque*self.niveau*2-adversaire.niveau
+        print("degat du magicien sur le méchant",degats)
+        #inflige des dégats au mechant si celui-ci est vivant
+        # et que le magicien dispose de nana
+        #incrémente le nombre de points d’expérience correspondant aux dégâts infligés
+        #Monte si nécessaire en niveau en fonction du nombre de points xp
+        #retire de la vie au méchant et diminue de 1 self.mana (consommation de magie)
+        #si le méchant est mort augmenter self.maxMana de 10 du magicien.
+        if adversaire.estVivant() and self.mana>0:
+            adversaire.vie -= degats
+            self.monterExperience(degats)
+            self.mana -= 1
+        if adversaire.estMort():
+            self.augmenterMana()
 
-p = Guerrier("ehoh",1,100,3,0)
+"""
+p = Guerrier("ehoh",1,100,13,1)
+maj = Magicien("gandalfe",10,100,10,1)
 adv = Personnage("Gentil",2,20,3)
-p.monterExperience(1)
+adv2 = Personnage("Gentil",2,20,3)
+p.monterExperience(10)
 p.ajouterVie(1)
-print(p.vie,p.xp,p.estVivant(),p.estMort())
-for i in range(10):
+while adv.estVivant():
     p.combat(adv)
+print(adv.estMort(),adv.vie)
 print(p.vie,p.xp,p.niveau,p.estVivant(),p.estMort())
-"""
+
 p.retirerVie(151)
-p.monterExperience()
-p.monterExperience()
-p.monterExperience()
-p.monterExperience()
+p.monterExperience(1)
+p.monterExperience(1)
+p.monterExperience(1)
+p.monterExperience(1)
+while adv2.estVivant():
+    maj.combat(adv2)
+print(maj.vie,maj.xp,maj.niveau,maj.estVivant(),maj.estMort())
 """
+combattant=Guerrier("Linflas",2,20,0,1)
+mechant=Guerrier("Chaos",2,30,0,2)
+magot=Magicien("Chani",30,30,0,2)
+def duel(combattant,mechant):
+    while mechant.estVivant() and combattant.estVivant():
+        combattant.combat(mechant)
+#Acompléter (utiliser une boucle tant que)
+duel(magot,mechant)
+print(combattant)
+print(magot)
+print(mechant)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
