2194f6f157b88edeb6aa7a6ed85eef0925cdb0da
eti-du <92732299+eti-du@users.noreply.github.com>
2022-10-06 19:00:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,30 +6,27 @@ class Personnage:
         self.maxVie=vie
         self.xp=xp
         self.niveau=niveau
-    def ajouterVie(self,vie):
-        #ajoute de la vie dans self.vie sans dépasser self.maxVie
+    def ajouterVie(self,vie): #ajoute de la vie dans self.vie sans dépasser self.maxVie
         if self.vie+vie >= self.maxVie:
             self.vie = self.maxVie
         else:
             self.vie += vie
-    def retirerVie(self,vie):
-        #retire de la vie dans self.vie sans être inférieur à 0
+    def retirerVie(self,vie): #retire de la vie dans self.vie sans être inférieur à 0
         if self.vie-vie <= 0:
             self.vie = 0
         else:
             self.vie -= vie
-    def monterExperience(self,xp):
-        #ajoute 2 points d’expérience
+    def monterExperience(self,xp): #ajoute des points d’expérience
         #Augmente d’un niveau tous les 10 xp
         #Exemple 30xp = niveau 3
         self.xp += xp
-        self.niveau = self.xp//10# ???
-    def estVivant(self):
-        #retourne vrai si le personnage est vivant
+        self.niveau = self.xp//10+1# ???
+    def estVivant(self): #retourne vrai si le personnage est vivant
         return self.vie>0
-    def estMort(self):
-        #retourne vrai si le personnage est mort
+    def estMort(self): #retourne vrai si le personnage est mort
         return self.vie<=0
+    def __repr__(self):
+        return "("+str(self.niveau)+") "+self.nom+" à "+str(self.vie)+" points de vie, soit est à "+str(100*self.vie//self.maxVie)+"% vivant et à "+str(self.xp)+" points d'expériences"
 
 class Guerrier(Personnage):
     def __init__(self,nom,force,vie,xp,niveau):
@@ -50,7 +47,7 @@ class Guerrier(Personnage):
         self.monterExperience(degats)
         if adversaire.estMort():
             self.augmenterForce()
-        print("degat sur le mechant",degats)
+        print("degat sur "+adversaire.nom,degats)
 class Magicien(Personnage):
     def __init__(self,nom,mana,vie,xp,niveau):
         super().__init__(nom,vie,xp,niveau)
@@ -73,7 +70,7 @@ class Magicien(Personnage):
     def combat(self,adversaire):
         attaque=randint(1, 4)
         degats=attaque*self.niveau*2-adversaire.niveau
-        print("degat du magicien sur le méchant",degats)
+        print("degat du magicien sur "+adversaire.nom,degats)
         #inflige des dégats au mechant si celui-ci est vivant
         # et que le magicien dispose de nana
         #incrémente le nombre de points d’expérience correspondant aux dégâts infligés
@@ -81,7 +78,7 @@ class Magicien(Personnage):
         #retire de la vie au méchant et diminue de 1 self.mana (consommation de magie)
         #si le méchant est mort augmenter self.maxMana de 10 du magicien.
         if adversaire.estVivant() and self.mana>0:
-            adversaire.vie -= degats
+            adversaire.retirerVie(degats)
             self.monterExperience(degats)
             self.mana -= 1
         if adversaire.estMort():
@@ -114,7 +111,13 @@ magot=Magicien("Chani",30,30,0,2)
 def duel(combattant,mechant):
     while mechant.estVivant() and combattant.estVivant():
         combattant.combat(mechant)
-#Acompléter (utiliser une boucle tant que)
+        mechant.combat(combattant)
+    if mechant.estMort():
+        return "Vous avez perdu le combat"
+    elif combattant.estMort():
+        return "Vous avez gagné le combat"
+    else:
+        return "erreur"
 duel(magot,mechant)
 print(combattant)
 print(magot)
