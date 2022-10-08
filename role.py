aaea0f3615f4580d144565bab501da7826d7cda5
eti-du <92732299+eti-du@users.noreply.github.com>
2022-10-08 20:11:30 +0200

    Update role.py

--- role.py
+++ role.py
@@ -5,53 +5,113 @@ import pygame
 from os.path import join,dirname
 
 #variables du niveau
-NB_TILES = 666   #nombre de tiles a chager (ici de 00.png à 26.png) 27 au total !!
-TITLE_SIZE=64   #definition du dessin (carré)
-largeur=8       #hauteur du niveau
-hauteur=8       #largeur du niveau
-tiles=[]       #liste d'images tiles
+NB_TILES = 666   #nombre de tuiles a charger (ici de 00.png à 666.png) 667 au total !
+TITLE_SIZE = 64  #definition du dessin (carré)
+largeur = 20     #hauteur du niveau
+hauteur = 12     #largeur du niveau
+tiles=[]         #liste des images des tuiles
 clock = pygame.time.Clock()
 
 
+#la taille de la fenetre dépend de la largeur et de la hauteur du niveau
+#on rajoute une rangée de 32 pixels en bas de la fentre pour afficher le score d'ou (hauteur +1)
+pygame.init()
+window = pygame.display.set_mode((0,0))
+#window = pygame.display.set_mode((largeur*TITLE_SIZE, (hauteur+1)*TITLE_SIZE))
+pygame.display.set_caption("Role Playing Game | The Mysterious Hill")
+font = pygame.font.Font('freesansbold.ttf', 20)
+fontG = pygame.font.Font('freesansbold.ttf', 120)
+window_x,window_y = pygame.display.Info().current_w,pygame.display.Info().current_h
+window.blit(fontG.render("CHARGEMENT …", True, (113,52,134)),(window_x//4,window_y//2-50))
+pygame.display.update()
+
 #definition du niveau
 
-niveau=[[ 23, 24, 24, 24, 24, 24, 24, 25],
+niveau = [
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47],
+[47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47]]
+"""[[ 23, 24, 24, 24, 24, 24, 24, 25],
         [189,189,171, 47, 47, 47, 47, 48],
         [ 46, 47,187, 47, 47, 47, 47, 48],
         [ 46,118,217,120, 47, 47, 47, 48],
         [ 46,141,142,143, 47, 47, 47, 48],
         [ 46,164,165,166, 47, 47, 47, 48],
         [506,507,507,507,507,507,507,508],
-        [529,486,486,486,486,486,486,531]]
+        [529,486,486,486,486,486,486,531]]"""
 
-decor=[[  0,  0,253,  0,  0,  0,  0,  0],
+decor = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+"""[[  0,  0,253,  0,  0,  0,  0,  0],
        [  0,  0,  0,  0,  0,  0,  0,  0],
        [184,  0,  0,138,  0,278,279,  0],
        [  0,  0,  0,  0,  0,276,277,  0],
        [  0,  0,  0,  0,  0,299,300,  0],
        [186,  0,  0,  0,  0,  0,  0,  0],
        [  0,  0,  0,  0,  0,  0,  0,  0],
-       [  0,  0,  0,  0,  0,  0,  0,  0]]
+       [  0,  0,  0,  0,  0,  0,  0,  0]]"""
 
 
-collisions=[[  1,  1,  1,  1,  1,  1,  1,  1],
+collisions = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+"""[[  1,  1,  1,  1,  1,  1,  1,  1],
             [  1,  0,  0,  0,  0,  0,  0,  1],
             [  1,  0,  0,  0,  0,  1,  1,  1],
             [  1,  0,  0,  0,  0,  1,  1,  1],
             [  1,  0,  0,  0,  0,  1,  1,  1],
             [  1,  0,  0,  0,  0,  0,  0,  1],
             [  1,  0,  0,  0,  0,  0,  0,  1],
-            [  1,  1,  1,  1,  1,  1,  1,  1]]
-
+            [  1,  1,  1,  1,  1,  1,  1,  1]]"""
 
 
 class Personnage(pygame.sprite.Sprite):
 
     def __init__(self,position,size,img,collisions):
         super().__init__()
-
-
-        self.image = pygame.image.load(img)
+        self.image = pygame.transform.scale(pygame.image.load(img),(64,64))
         self.rect = self.image.get_rect()
         self.size=size
         self.collisions=collisions
@@ -81,17 +141,6 @@ class Personnage(pygame.sprite.Sprite):
         self.rect.y=self.y*self.size
 
 
-
-
-#la taille de la fenetre dépend de la largeur et de la hauteur du niveau
-#on rajoute une rangée de 32 pixels en bas de la fentre pour afficher le score d'ou (hauteur +1)
-pygame.init()
-fenetre = pygame.display.set_mode((largeur*TITLE_SIZE, (hauteur+1)*TITLE_SIZE))
-pygame.display.set_caption("Dungeon | RPG")
-font = pygame.font.Font('freesansbold.ttf', 20)
-
-
-
 def chargetiles(tiles):
     """
     fonction permettant de charger les images tiles dans une liste tiles[]
@@ -100,32 +149,24 @@ def chargetiles(tiles):
         #print('data/'+str(n)+'.png')
         tiles.append(pygame.transform.scale(pygame.image.load(join(dirname(__file__),'data/'+str(n)+'.png')),(64,64))) #attention au chemin
 
-
 def afficheNiveau(niveau):
     """
     affiche le niveau a partir de la liste a deux dimensions niveau[][]
     """
     for y in range(hauteur):
         for x in range(largeur):
-            fenetre.blit(tiles[niveau[y][x]],(x*TITLE_SIZE,y*TITLE_SIZE))
+            window.blit(tiles[niveau[y][x]],(x*TITLE_SIZE,y*TITLE_SIZE))
             if (decor[y][x]>0):
-                fenetre.blit(tiles[decor[y][x]],(x*TITLE_SIZE,y*TITLE_SIZE))
-
-
+                window.blit(tiles[decor[y][x]],(x*TITLE_SIZE,y*TITLE_SIZE))
 
 def afficheScore(score):
     """
     affiche le score
     """
-    #exemple bidon
+    #exemple
     #scoreAafficher = font.render(str(score), True, (0, 255, 0))
-    #fenetre.blit(scoreAafficher,(120,250))
+    #window.blit(scoreAafficher,(120,250))
     pass
-
-
-
-fenetre.fill((0,0,0))   #efface la fenetre
-chargetiles(tiles)  #chargement des images
 
 
 perso = Personnage([1,1],TITLE_SIZE,join(dirname(__file__),"data/perso.png"),collisions)
@@ -141,6 +182,7 @@ mechants.add(perso2)
 
 
 
+chargetiles(tiles)#charge les images
 loop=True
 while loop==True:
     for event in pygame.event.get():
@@ -158,19 +200,20 @@ while loop==True:
             elif event.key == pygame.K_ESCAPE or event.unicode == 'q': #touche q pour quitter
                 loop = False
     col = pygame.sprite.collide_rect(perso, perso2)
-    if col==1:
-        print("collision",col)
+    if col:
+        perso.gauche()
+        print("collision")
         mechants.remove(perso2)
 
 
-    fenetre.fill((0,0,0))
+    window.fill((0,0,0))
     afficheNiveau(niveau)   #affiche le niveau
     aventuriers.update()
-    aventuriers.draw(fenetre)
+    aventuriers.draw(window)
     mechants.update()
-    mechants.draw(fenetre)
-    pygame.display.update() #mets à jour la fentre graphique
+    mechants.draw(window)
+    pygame.display.update() #mets à jour la fenetre graphique
     #pygame.display.flip()
-    #clock.tick(60)
+    clock.tick(60)
 pygame.quit()
 
