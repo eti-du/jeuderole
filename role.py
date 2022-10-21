6e750008edb0910c238aed796e74f32631af165a
eti-du <92732299+eti-du@users.noreply.github.com>
2022-10-21 08:52:33 +0200

    Update role.py

--- role.py
+++ role.py
@@ -7,20 +7,23 @@ from os.path import join,dirname
 #variables
 NB_TILES = 666   #nombre de tuiles a charger (ici de 00.png à 666.png) 667 au total !
 TILE_SIZE = 64   #definition du dessin (carré)
-largeur = 20     #hauteur du niveau
-hauteur = 12     #largeur du niveau
 tiles=[]         #liste des images des tuiles
 clock = pygame.time.Clock()
 #La taille de la fenetre ne dépend pas de la largeur et de la hauteur du niveau
 #On rajoute une rangée de quelques pixels en bas de la fentre pour afficher le score
 pygame.init()
-window = pygame.display.set_mode((0,0)) #window = pygame.display.set_mode((largeur*TILE_SIZE, (hauteur+1)*TILE_SIZE))
+window = pygame.display.set_mode((0,0),flags=pygame.FULLSCREEN) #window = pygame.display.set_mode((largeur*TILE_SIZE, (hauteur+1)*TILE_SIZE))
 pygame.display.set_caption("Role Playing Game | The Mysterious Hill")
-font = pygame.font.Font('freesansbold.ttf', 20)
+font = pygame.font.Font('freesansbold.ttf', 40)
 fontG = pygame.font.Font('freesansbold.ttf', 120)
 window_x,window_y = pygame.display.Info().current_w,pygame.display.Info().current_h
 window.blit(fontG.render("CHARGEMENT …", True, (113,52,134)),(window_x//4,window_y//2-50))
 pygame.display.update()
+
+largeur = min(20,window_x//TILE_SIZE-5) #hauteur du niveau
+hauteur = min(14,window_y//TILE_SIZE-3) #largeur du niveau
+
+
 
 #definition du terrain
 niveau = [
@@ -151,10 +154,129 @@ def afficheScore(score):
     affiche le score
     """
     #exemple
-    #scoreAafficher = font.render(str(score), True, (0, 255, 0))
-    #window.blit(scoreAafficher,(120,250))
+    scoreAafficher = font.render(str(score), True, (20, 235, 134))
+    window.blit(scoreAafficher,(10,window_y-64))
     pass
+#==Personnages==
+from random import randint,choice
+class Personnage2:
+    def __init__(self,nom,vie,xp,niveau):
+        self.nom=nom
+        self.vie=vie
+        self.maxVie=vie
+        self.xp=xp
+        self.niveau=niveau
+    def ajouterVie(self,vie):#ajoute de la vie dans self.vie sans dépasser self.maxVie
+        if self.vie+vie >= self.maxVie:
+            self.vie = self.maxVie
+        else:
+            self.vie += vie
+    def retirerVie(self,vie):#retire de la vie dans self.vie sans être inférieur à 0
+        if self.vie-vie <= 0:
+            self.vie = 0
+        else:
+            self.vie -= vie
+    def monterExperience(self,xp):#ajoute 2 points d’expérience, Augmente d’un niveau tous les 10 xp, Exemple 30xp = niveau 3
+        self.xp += xp
+        self.niveau = self.xp//10
+    def estVivant(self):
+        #retourne vrai si le personnage est vivant
+        return self.vie>0
+    def estMort(self):
+        #retourne vrai si le personnage est mort
+        return self.vie<=0
 
+class Guerrier(Personnage):
+    def __init__(self,nom,force,vie,xp,niveau):
+        super().__init__(nom,vie,xp,niveau)
+        self.force=force
+    def augmenterForce(self):
+        self.force += 1
+    def combat(self,adversaire):
+        #inflige des dégats au mechant si celui-ci est vivant
+        #incrémente le nombre de points d’expérience correspondant aux dégâts infligés
+        #Monte si nécessaire en niveau en fonction du nombre de points xp
+        #retire de la vie au méchant
+        #si le méchant est mort augmenter la force de 1 du guerrier
+        attaque=randint(1, 4)
+        degats=attaque*self.niveau*self.force-adversaire.niveau
+        if adversaire.estVivant():
+            adversaire.retirerVie(degats)
+        self.monterExperience(degats)
+        if adversaire.estMort():
+            self.augmenterForce()
+        print("degat sur le mechant",degats)
+class Magicien(Personnage):
+    def __init__(self,nom,mana,vie,xp,niveau):
+        super().__init__(nom,vie,xp,niveau)
+        self.maxMana=mana
+        self.mana=mana
+    def augmenterMana(self):#augmente de 10 self.maxMana
+        self.maxMana += 10
+    def ajouterMana(self):#ajoute 1 en self.mana sans dépasser self.maxMana
+        if self.mana + 1 <= self.maxMana:
+            self.mana += 1
+    def retirerMana(self,mana):
+        #retire mana à self.mana sans descendre en dessous de 0
+        #retourne vrai si le magicien à lancé un sort
+        #retourne faux si le magicien nen peut plus lancer de sort.
+        if self.mana -1 >= 0:
+            self.mana -= 1
+            return True
+        else:
+            return False
+    def combat(self,adversaire):
+        attaque=randint(1, 4)
+        degats=attaque*self.niveau*2-adversaire.niveau
+        print("degat du magicien sur le méchant",degats)
+        #inflige des dégats au mechant si celui-ci est vivant
+        # et que le magicien dispose de nana
+        #incrémente le nombre de points d’expérience correspondant aux dégâts infligés
+        #Monte si nécessaire en niveau en fonction du nombre de points xp
+        #retire de la vie au méchant et diminue de 1 self.mana (consommation de magie)
+        #si le méchant est mort augmenter self.maxMana de 10 du magicien.
+        if adversaire.estVivant() and self.mana>0:
+            adversaire.vie -= degats
+            self.monterExperience(degats)
+            self.mana -= 1
+        if adversaire.estMort():
+            self.augmenterMana()
+
+"""
+p = Guerrier("ehoh",1,100,13,1)
+maj = Magicien("gandalfe",10,100,10,1)
+adv = Personnage("Gentil",2,20,3)
+adv2 = Personnage("Gentil",2,20,3)
+p.monterExperience(10)
+p.ajouterVie(1)
+print(adv.estMort(),adv.vie)
+print(p.vie,p.xp,p.niveau,p.estVivant(),p.estMort())
+
+p.retirerVie(151)
+p.monterExperience(1)
+p.monterExperience(1)
+p.monterExperience(1)
+p.monterExperience(1)
+while adv2.estVivant():
+    maj.combat(adv2)
+print(maj.vie,maj.xp,maj.niveau,maj.estVivant(),maj.estMort())
+"""
+def duel(combattant,mechant):
+    i = 0
+    while mechant.estVivant() and combattant.estVivant() or i>2000:
+        combattant.combat(mechant)
+        mechant.combat(combattant)
+        i += 1
+"""
+duel(magot,mechant)
+print(combattant)
+print(magot)
+print(mechant)"""
+
+combattant=Guerrier("Linflas",2,20,10,1)
+mechant=Guerrier("Chaos",2,30,10,2)
+magot=Magicien("Chani",30,30,10,2)
+#==Fin personnages==
 #création des personnages
 perso = Personnage([1,1],TILE_SIZE,join(dirname(__file__),"data/perso.png"),collisions)
 perso2 = Personnage([3,3],TILE_SIZE,join(dirname(__file__),"data/perso.png"),collisions)
@@ -193,6 +315,7 @@ while loop==True:
 
     window.fill((0,0,0))
     afficheNiveau(niveau) #affiche le niveau
+    afficheScore("Score")
     aventuriers.update()
     aventuriers.draw(window)
     mechants.update()
